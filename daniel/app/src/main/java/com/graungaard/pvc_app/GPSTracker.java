72915237315b62daeb18476a07466e1ce3c31ba0
G74un3 <danielgraungaard@gmail.com>
2014-09-25 16:28:30 +0200

    GPS tracker cleanup

--- daniel/app/src/main/java/com/graungaard/pvc_app/GPSTracker.java
+++ daniel/app/src/main/java/com/graungaard/pvc_app/GPSTracker.java
@@ -1,12 +1,12 @@
 package com.graungaard.pvc_app;
 
 
-import android.location.LocationListener;
 import android.app.AlertDialog;
 import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,15 +14,15 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.util.Log;
+
 /**
  * Created by anders on 23-09-2014.
  */
 public class GPSTracker extends Service implements LocationListener {
 
     private final Context mContext;
-    boolean isGPSEnabled =false;
+    boolean isGPSEnabled = false;
     boolean isNetworkEnabled = false;
-    boolean canGetLocation = false;
 
     Location location;
     double latitude;
@@ -31,7 +31,7 @@ public class GPSTracker extends Service implements LocationListener {
     //Minimum distancen for at ændre updates i meter. Den er sat til 10 meter.
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
     //Minimum tid mellem updates i millisekunder. Den er sat til 1 min.
-    private static final long  MIN_TIME_BW_UPDATES = 1000*60*1;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
     protected LocationManager locationManager;
 
 
@@ -41,81 +41,68 @@ public class GPSTracker extends Service implements LocationListener {
     }
 
 
-    public Location getLocation(){
-        try{
-            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+    public Location getLocation() {
+        try {
 
-            //Får GPS status
-            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            initiateLocation();
 
-            //Får netværks status
-            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-            if(!isGPSEnabled && !isNetworkEnabled){
-                //ingen netværks provider er aktiveret
+            if (!isGPSEnabled && !isNetworkEnabled) {
 
-            }else{
-                this.canGetLocation = true;
-                //Først få lokation fra Netværks provideren
-                if(isNetworkEnabled){
-                    locationManager.requestLocationUpdates(
-                            locationManager.NETWORK_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                    Log.d("Network", "Network");
-                    if (locationManager !=null){
-                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                        if (location != null) {
-                            latitude = location.getLatitude();
-                            longitude = location.getLongitude();
-                        }
-                    }
-                }
+                Log.d("NO GPS,NO NETWORK", "NO GPS, NO NETWORK");
+
+            } else {
+
+                Criteria criteria = new Criteria();
+
+                String bestprovider = locationManager.getBestProvider(criteria, true);
+
+                Location location = locationManager.getLastKnownLocation(bestprovider);
+
+                Log.w("LOCATION HEJ", location + "");
+
             }
-            //hvis GPS er aktiveret få lat/long ved brug a GPS service
-            if (isGPSEnabled){
-                if(location == null){
-                    locationManager.requestLocationUpdates(
-                            locationManager.GPS_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                    Log.d("GPS Enabled", "GPS Enabled");
-                    if(locationManager != null){
-                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        if(location != null){
-                            latitude = location.getLatitude();
-                            longitude = location.getLongitude();
-                        }
-                    }
-                }
-            }
-        }catch (Exception e){
+
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+
         return location;
+    }
+
+
+    private void initiateLocation() {
+
+
+        locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+
+        //Får GPS status
+        isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        //Får netværks status
+        isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
     }
 
     //Function to get latitude
 
-    public double getLatitude(){
-        if(location != null){
+    public double getLatitude() {
+        if (location != null) {
             latitude = location.getLatitude();
         }
         return latitude;
     }
 
-    public double getLongitude(){
-        if(location != null){
+    public double getLongitude() {
+        if (location != null) {
             longitude = location.getLongitude();
         }
         return longitude;
     }
 
 
-    public boolean canGetLocation(){
-        return this.canGetLocation;
-    }
-
-    public void showSettingsAlert(){
+    public void showSettingsAlert() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
 
         //Settings to dialog title
@@ -130,16 +117,16 @@ public class GPSTracker extends Service implements LocationListener {
 
         //Når der bliver trykket på settings knappen
 
-        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialog,int which){
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
                 Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 mContext.startActivity(intent);
             }
-            });
+        });
 
         //Når der bliver trykket på cancel
-        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialog, int which){
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
             }
         });
@@ -150,17 +137,15 @@ public class GPSTracker extends Service implements LocationListener {
     }
 
     //Denne function stopper gps'en hvis den er kaldt
-    public void stopUsingGPS(){
-        if(locationManager != null){
+    public void stopUsingGPS() {
+        if (locationManager != null) {
             locationManager.removeUpdates(GPSTracker.this);
         }
     }
 
 
-
-
     @Override
-        public void onLocationChanged(Location location) {
+    public void onLocationChanged(Location location) {
 
     }
 
