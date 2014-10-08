2acbe5d82c44250799ab4f04ee99a4216d774131
unknown <salban_k@hotmail.com>
2014-10-08 21:27:21 +0200

    GUI delene, med BEGGE afslutninger er done.

--- daniel/app/src/main/java/com/graungaard/pvc_app/PoliceSuccesActivity.java
+++ daniel/app/src/main/java/com/graungaard/pvc_app/PoliceSuccesActivity.java
@@ -1,27 +1,24 @@
 package com.graungaard.pvc_app;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class PoliceActivity extends Activity {
+public class PoliceSuccesActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_police);
+        setContentView(R.layout.activity_police_succes);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.police, menu);
+        getMenuInflater().inflate(R.menu.police_succes, menu);
         return true;
     }
 
@@ -35,10 +32,5 @@ public class PoliceActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public void buttonOnClick(View v) {
-        Button button = (Button) v;
-        startActivity(new Intent(this, PoliceMessageActivity.class));
     }
 }
